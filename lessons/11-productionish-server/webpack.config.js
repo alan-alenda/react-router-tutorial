66b74d0b2c691812157fed603548c412000fcd96
Alan Edwards <alan@alenda.ie>
2018-01-01 07:45:45 +0000

    completed

--- lessons/11-productionish-server/webpack.config.js
+++ lessons/11-productionish-server/webpack.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+
+  // add this handful of plugins that optimize the build
+  // when we're in production
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ] : [],
+
   entry: './index.js',
   
   output: {
